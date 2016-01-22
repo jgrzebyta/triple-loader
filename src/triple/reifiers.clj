cf59f05f0488654d920e0215151821697305cf2a
Jacek Grzebyta <jgrzebyta@users.noreply.github.com>
2016-01-22 15:03:04 +0000

    create instance of java's RDFHandler

--- src/triple/reifiers.clj
+++ src/triple/reifiers.clj
@@ -1,9 +1,26 @@
 (ns triple.reifiers
   (:gen-class)
-  (:import [org.openrdf.rio RDFHandler]
-           [org.openrdf.repository RepositoryConnection]
+  (:import [org.openrdf.rio RDFHandler RDFHandlerException]
+           [org.openrdf.repository RepositoryConnection RepositoryException]
            [org.openrdf.repository.util RDFInserter]))
 
 
-(defn reify-rdf-handler "implement RDFHandler" []
-  nil)
+(def chunk-size 1000)
+
+(defn chunk-commiter "implement RDFHandler"
+  [connection]
+  (let [inserter (RDFInserter. connection)
+        count 0]
+    (reify RDFHandler
+      (startRDF [this] (.startRDF inserter))
+      (endRDF [this] (.endRDF inserter))
+      (handleComment [this comment] (.handleComment inserter comment))
+      (handleNamespace [this prefix uri] (.handleNamespace inserter prefix uri))
+      (handleStatement [this statement] (do
+                                       (.handleStatement inserter statement)
+                                       (inc count)
+                                       (if (= 0 (mod count chunk-size))
+                                         (try
+                                           (.commit connection)
+                                           (catch RepositoryException e (RDFHandlerException. e))
+                                         )))))))
