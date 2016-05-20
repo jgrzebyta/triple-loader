81672594f05606cc3dce7f853cae2a352ab11618
Jacek Grzebyta <jgrzebyta@users.noreply.github.com>
2016-05-20 18:06:28 +0100

    remove doubled definition of iter-seq

--- src/triple/repository.clj
+++ src/triple/repository.clj
@@ -87,11 +87,3 @@ Code was adapted from kr-sesame: sesame-context-array."
                   (boolean use-reified)
                   context))
 
-
-
-(defn iter-seq "It is iterator-seq like but works with iterator-like patterns.
-Reused implementation describe in http://stackoverflow.com/questions/9225948/ task."
-  [i] 
-    (lazy-seq 
-      (when (.hasNext i)
-        (cons (.next i) (iter-seq i)))))
